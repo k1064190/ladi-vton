3ffc37c350e430492fd94c851c5f55c6b2604daa
Gagasbandas <kjjcds@gmail.com>
2023-11-21 19:24:45 +0900

    impl) predict_pose가 body_25를 이용하도록 수정, human parser의 이미지가 palette를 거치도록 수정

--- Self-Correction-Human-Parsing/simple_extractor.py
+++ Self-Correction-Human-Parsing/simple_extractor.py
@@ -145,7 +145,7 @@ def main():
             parsing_result = np.argmax(logits_result, axis=2)
             parsing_result_path = os.path.join(args.output_dir, img_name[:-4] + '.png')
             output_img = Image.fromarray(np.asarray(parsing_result, dtype=np.uint8))
-            # output_img.putpalette(palette)
+            output_img.putpalette(palette)
             output_img.save(parsing_result_path)
 
             if args.logits:
--- predict_pose.py
+++ predict_pose.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import json
 
-class general_pose_model(object):
+class general_coco_model(object):
     def __init__(self, modelpath):
         # Specify the model to be used
         #   Body25: 25 points
@@ -81,10 +81,84 @@ class general_pose_model(object):
 
         return points
 
+class general_body25_model(object):
+    def __init__(self, modelpath):
+        # Specify the model to be used
+        #   Body25: 25 points
+        #   COCO:   18 points
+        #   MPI:    15 points
+        self.inWidth = 368
+        self.inHeight = 368
+        self.threshold = 0.1
+        self.pose_net = self.general_body25_model(modelpath)
+
+    def general_body25_model(self, modelpath):
+        self.points_name = {
+            "Nose": 0, "Neck": 1,
+            "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+            "LShoulder": 5, "LElbow": 6, "LWrist": 7,
+            "MidHip": 8, "RHip": 9, "RKnee": 10,
+            "RAnkle": 11, "LHip": 12, "LKnee": 13,
+            "LAnkle": 14, "REye": 15, "LEye": 16,
+            "REar": 17, "LEar": 18, "LBigToe": 19,
+            "LSmallToe": 20, "LHeel": 21, "RBigToe": 22,
+            "RSmallToe": 23, "RHeel": 24, "Background": 25}
+        self.num_points = 25
+        prototxt   = os.path.join(
+            modelpath,
+            'pose_deploy.prototxt')
+        caffemodel = os.path.join(
+            modelpath,
+            'pose_iter_584000.caffemodel')
+        body25_model = cv2.dnn.readNetFromCaffe(prototxt, caffemodel)
+
+        return body25_model
+
+    def predict(self, imgfile):
+        img_cv2 = cv2.imread(imgfile)
+        frame = img_cv2.copy()
+        img_height, img_width, _ = img_cv2.shape
+        inpBlob = cv2.dnn.blobFromImage(img_cv2,
+                                        1.0 / 255,
+                                        (self.inWidth, self.inHeight),
+                                        (0, 0, 0),
+                                        swapRB=False,
+                                        crop=False)
+        self.pose_net.setInput(inpBlob)
+        self.pose_net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+        self.pose_net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL)
+
+        output = self.pose_net.forward()
+
+        H = output.shape[2]
+        W = output.shape[3]
+
+        points = []
+        for idx in range(self.num_points):
+            probMap = output[0, idx, :, :]  # confidence map.
+
+            # Find global maxima of the probMap.
+            minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+
+            # Scale the point to fit on the original image
+            x = (img_width * point[0]) / W
+            y = (img_height * point[1]) / H
+
+            if prob > self.threshold:
+                points.append(x)
+                points.append(y)
+                points.append(prob)
+            else:
+                points.append(0)
+                points.append(0)
+                points.append(0)
+        return points
+
+
 def generate_pose_keypoints(img_file, pose_file):
 
     modelpath = 'pose'
-    pose_model = general_pose_model(modelpath)
+    pose_model = general_body25_model(modelpath)
 
     res_points = pose_model.predict(img_file)
 
